f505c3d04601b95c335f985b50333491ea1ae768
Alvira Puteri <alvira.puteri@gdn-commerce.com>
2022-01-09 22:44:03 +0700

    change logic to start video in cucumber runner

--- src/test/java/com/blibli/future/CucumberRunner.java
+++ src/test/java/com/blibli/future/CucumberRunner.java
@@ -6,11 +6,10 @@ import com.blibli.future.data.PokemonJsonData;
 import com.blibli.future.data.PokemonListData;
 import com.blibli.future.utils.Utility;
 import com.blibli.future.utils.VideoRecorder_utlity;
-import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.junit.Before;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 import java.util.List;
 
@@ -24,15 +23,15 @@ import java.util.List;
 public class CucumberRunner extends AbstractTestNGCucumberTests{
     Utility utility = new Utility();
 
-    @Before
-    public  void before(Scenario scenario) throws Exception {
-        if (scenario.getSourceTagNames().equals("Website")) VideoRecorder_utlity.startRecord("WebsiteTestRecording");
-    }
-
-//    @BeforeClass
-//    public void beforeClass() throws Exception {
-//        VideoRecorder_utlity.startRecord("WebsiteTestRecording");
+//    @Before
+//    public  void before(Scenario scenario) throws Exception {
+//        if (scenario.getSourceTagNames().equals("Website")) VideoRecorder_utlity.startRecord("WebsiteTestRecording");
 //    }
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        VideoRecorder_utlity.startRecord("WebsiteTestRecording");
+    }
 
     @AfterClass
     public void afterClass() throws Exception {
